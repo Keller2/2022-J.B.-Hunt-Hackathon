7e590d405e8d6d0c1389e58fec39a55e7d845388
Keller2 <46767006+Keller2@users.noreply.github.com>
2022-04-04 19:48:50 -0500

    Add files via upload
    The other half of this project involves C# but will not be open-sourced

---
+++ Razbot.py
@@ -0,0 +1,35 @@
+import smtplib
+from time import sleep
+from turtle import st
+from email.mime.text import MIMEText
+print("Welcome To RazBot")
+sender_email = input(str("google username:"))
+password2 = input(str("Password:"))
+reciever_email = input(str("Send to:"))
+Subjectz = input(str("Subject line:"))
+From = input(str("From (the name you want to show up next to the email address): Currently not working because im forcing it to be one name go into the code and edit the line that says msg['from']"))
+#smtplib.set_debuglevel(1) #Remove this if you need more information if code isnt working
+TEXT = input(str("Email body:"))
+SUBJECT = "test"   
+#def mail_me(cont, receiver):
+msg = MIMEText(TEXT, 'html')
+recipients = ",".join(reciever_email)
+msg['Subject'] = Subjectz
+msg['From'] = '"Test"'
+#msg['To'] = recipients
+server = smtplib.SMTP_SSL('smtp.gmail.com')
+
+
+server.login(sender_email, password2)
+print("Login complete")
+server.sendmail('RazALERT', reciever_email, msg.as_string())
+print("Email sent")
+sleep(2000)
+print("Enter another email to send another Alert:")
+reciever_email = input(str("Send to:"))
+server.sendmail('RazALERT', reciever_email, msg.as_string())
+print("Email sent")
+print("Enter another email to send another Alert:")
+reciever_email = input(str("Send to:"))
+server.sendmail('RazALERT', reciever_email, msg.as_string())
+print("Email sent")
